09fd1619d7dfd8dba0a8600e1eb7071f7d34292e
dxd8201 <ddronet17@gmail.com>
2023-03-08 21:16:22 -0600

    Created starter files. created function to read through book array and displays content added starter css to show book cards added new book button to pull up a form to add a new book created book form template to be used

---
+++ script.js
@@ -0,0 +1,49 @@
+const bookContainer = document.querySelector("#bookContainer");
+const newBookBtn = document.querySelector("#newBookBtn");
+
+let myLibrary = [
+    {
+        "title": "Harry Potter",
+        "pages": 126,
+        "author": "jk Rowling"
+    },
+    {
+        "title": "the hobbit",
+        "pages": 198,
+        "author": "Tolkein"
+    }
+];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+    for(i=0; i < myLibrary.length; i++) {
+        console.table(myLibrary);
+        let myLibraryData = myLibrary[i];
+        let newBook = document.createElement("div");
+        let titleElt = document.createElement("p");
+        let pagesElt = document.createElement("p");
+        let authorElt = document.createElement("p");
+
+        //appends each element to the correct parent
+        bookContainer.appendChild(newBook);
+        newBook.appendChild(titleElt);
+        newBook.appendChild(pagesElt);
+        newBook.appendChild(authorElt);
+
+        //add the respective class to the specified element
+        newBook.classList.add("book-card")
+        titleElt.classList.add("title-container");
+        pagesElt.classList.add("pages-container");
+        authorElt.classList.add("author-container");
+
+        //fills the element with the object info
+        titleElt.textContent = myLibraryData.title;
+        pagesElt.textContent = myLibraryData.pages;
+        authorElt.textContent = myLibraryData.author;
+    }
+}
+
+addBookToLibrary();
